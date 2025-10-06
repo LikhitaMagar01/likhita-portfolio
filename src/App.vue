3fed1a7d1b78e0af977c30a1ca2e6a81ec66cdc8
Likhita (LA) <80843873+LikhitaMagar01@users.noreply.github.com>
2025-10-06 21:53:47 +0545

    update style

--- src/App.vue
+++ src/App.vue
@@ -34,7 +34,7 @@ function handleSectionChange(section: string) {
             <div class="text-xs text-gray-300 mb-3">{{ profile.location }}</div>
             <button
               class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg w-full font-semibold flex items-center justify-center gap-2">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M3 3H21C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3ZM12.0606 11.6829L5.64722 6.2377L4.35278 7.7623L12.0731 14.3171L19.6544 7.75616L18.3456 6.24384L12.0606 11.6829Z"></path></svg> {{ profile.contactButton }}
+              <svg style="width: 24px; height: 24px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M3 3H21C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3ZM12.0606 11.6829L5.64722 6.2377L4.35278 7.7623L12.0731 14.3171L19.6544 7.75616L18.3456 6.24384L12.0606 11.6829Z"></path></svg> {{ profile.contactButton }}
             </button>
             <div class="text-[10px] text-gray-500 mt-4">{{ profile.copyright }}</div>
           </div>
